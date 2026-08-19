62ced676c368ad3be708a9801650b40ee97efba6
ybertoldi <yuri.bertoldi003@gmail.com>
2026-08-19 11:29:22 -0300

    feat: ultima janela salva

--- src/state/navigation.ts
+++ src/state/navigation.ts
@@ -1,6 +1,11 @@
-import { createSignal } from "solid-js";
+import { createEffect, createSignal } from "solid-js";
 import SIDEBAR_ROUTES from "../consts/sidebar_routes";
 
+
 export const [activeScreen, setActiveScreen] = createSignal(
-  SIDEBAR_ROUTES[0]?.id ?? ""
+  localStorage.getItem("last-screen") || SIDEBAR_ROUTES[0]?.id
 );
+
+createEffect(() => {
+  localStorage["last-screen"] = activeScreen()
+});
